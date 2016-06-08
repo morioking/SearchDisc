4c9eb8646147575046c99153924f2ea607268144
morioking <morioking@gmail.com>
2016-06-08 12:54:32 +0900

    開発の進行
    - shop_listを辞書(ディクショナリ)オブジェクトに変更
    - connect_jinjaを新規追加

--- sd.py
+++ sd.py
@@ -12,14 +12,14 @@ import shutil
 DEBUG = True
 
 shop_list=[
-#'http://www.mp3va.com/',
-#'https://www.beatport.com/',
-#'https://www.wasabeat.jp/',
-#'http://www.amazon.co.jp/MP3-%E3%83%80%E3%82%A6%E3%83%B3%E3%83%AD%E3%83%BC%E3%83%89-%E9%9F%B3%E6%A5%BD%E9%85%8D%E4%BF%A1-DRM%E3%83%95%E3%83%AA%E3%83%BC/b/ref=sd_allcat_mp3_str?ie=UTF8&node=2128134051',
-#'http://www.junodownload.com/',
-'https://hardwax.com/',
-#'http://www.jetsetrecords.net/',
-'http://diskunion.net/'
+#'shopname':u'mp3va', 		'shopurl':u'http://www.mp3va.com/',
+#'shopname':u'Beatport', 	'shopurl':u'https://www.beatport.com/',
+#'shopname':u'Wasabeat',	'shopurl':u'https://www.wasabeat.jp/',
+#'shopname':u'Amazon',		'shopurl':u'http://www.amazon.co.jp/MP3-%E3%83%80%E3%82%A6%E3%83%B3%E3%83%AD%E3%83%BC%E3%83%89-%E9%9F%B3%E6%A5%BD%E9%85%8D%E4%BF%A1-DRM%E3%83%95%E3%83%AA%E3%83%BC/b/ref=sd_allcat_mp3_str?ie=UTF8&node=2128134051',
+#'shopname':u'Juno',		'shopurl':u'http://www.junodownload.com/',
+{'shopname':u'Hardwax','shopurl':u'https://hardwax.com/'},
+#'shopname':u'JetSet',		'shopurl':u'http://www.jetsetrecords.net/',
+{'shopname':u'ユニオン','shopurl':u'http://diskunion.net/'}
 ]
 
 
@@ -28,20 +28,20 @@ titles=[]
 
 
 class Shop:
-	def __init__(self, url):
-		self.name=url
+	def __init__(self, name, url):
+		self.name=name
 		self.url=url
 		self.disk=[]
 		#self.createDiskList()
 		self.createDiskList4Test()
 
-	def getName(self):
+	def getShopName(self):
 		return self.name
 
-	def getUrl(self):
+	def getShopUrl(self):
 		return self.url
 
-	def getDiskList(self):
+	def getShopDiskList(self):
 		return self.disk
 
 	def createDiskList(self):
@@ -88,40 +88,67 @@ def create_html():
 	f.write(html.encode('utf-8'))
 	f.close()
 
+def connect_jinja(c):
+	# env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
+	# tmpl = env.get_template('jinja.tmpl')
+	# title = u"sd.py result"
+
+	for shop_class in c:
+		print shop_class.getShopName()
+		print shop_class.getShopUrl()
+		disk_class_list = shop_class.getShopDiskList()
+		for disk_class in disk_class_list:
+			print disk_class.getDiskTitle()
+			print disk_class.getDiskUrl()
+	
+	# sample_list = []
+	# sample_list.append({'shopname':u"Hard Wax", 'shopurl':u"https://hardwax.com/"})
+	# sample_list.append({'name':u"Kassem Mosse: Workshop 12 - Hard Wax", 'url':u"https://hardwax.com/62572/kassem-mosse/workshop-12/"})
+	# sample_list.append({'name':u"Kassem Mosse: Workshop 08 - Hard Wax", 'url':u"https://hardwax.com/58639/kassem-mosse/workshop-08"})
+	# sample_list.append({'name':u"Kassem Mosse: Workshop 03 - Hard Wax", 'url':u"https://hardwax.com/54697/kassem-mosse/workshop-03/"})
+	# sample_list.append({'shopname':u"disk union", 'shopurl':u"http://diskunion.net/"})
+	# sample_list.append({'name':u"KASSEM MOSSE / Workshop 12 | diskunion.net PROGRESSIVE ROCK ONLINE SHOP", 'url':u"http://diskunion.net/progre/ct/detail/CM-0036556?utm_source=rmd&utm_medium=ad&utm_campaign=rmd"})
+	# sample_list.append({'name':u"Workshop 12 - KASSEM MOSSE - 12&quot;(レコード) | CLUB/DANCE | ディスクユニオン･オンラインショップ", 'url':u"http://diskunion.net/sp/punk/detail/CM-0036556?utm_source=rmd&utm_medium=ad&utm_campaign=rmd"})
+	# html = tmpl.render({'title':title, 'disk_list':sample_list})
+	# f = open('jinja.html', 'w')
+	# f.write(html.encode('utf-8'))
+	# f.close()
+
 
 if __name__ == "__main__":
+
+	shop_class_list=[]
+		
 	if DEBUG:
 		print 'DEBUG==true'
+		HardWax = Shop("HardWax", "https://hardwax.com/")
+		DiskUnion = Shop("DiskUnion", "http://diskunion.net/")
+		shop_class_list.append(HardWax)
+		shop_class_list.append(DiskUnion)
 	else:
+		input_line1 = raw_input()
 		print 'DEBUG==false'
 		
+	#connect_jinja(shop_class_list)
 
-	HardWax = Shop("https://hardwax.com/")
-	DiskUnion = Shop("http://diskunion.net/")
-	for c in HardWax.getDiskList():
-		print c.getDiskUrl(),c.getDiskTitle()
 
-	for c in DiskUnion.getDiskList():
-		print c.getDiskUrl(),c.getDiskTitle()
-
+	for a in shop_list:
+		print a['shopname'], a['shopurl']
+	
+	
 	#create_html()
 
-	print "input words..."
-	#input_line1 = raw_input()
-	#input_line1 = 'kassem mosse workshop 12'
-
-	# pack=[]
 	# for shop_url in shop_list:
-		# pack.append(Shop(shop_url))
+		# shop_class_list.append(Shop(shop_url))
 
 	# f = open('test.html', 'w')
 	# f.write("<html>")
 	# f.write("<body>")
 
-	# for shop_class in pack:
-		# html = u"<p><A Href="+shop_class.getUrl()+">"+shop_class.getName()+"</A></p>"
+	# for shop_class in shop_class_list:
+		# html = u"<p><A Href="+shop_class.getShopUrl()+">"+shop_class.getShopName()+"</A></p>"
 		# f.write(html)
-		# for disk in shop_class.getDiskList():
+		# for disk in shop_class.getShopDiskList():
 			# html = u"<p><A Href="+disk.getDiskUrl()+">"+disk.getDiskTitle()+"</A></p>"
 			# f.write(html.encode('utf-8'))
 
