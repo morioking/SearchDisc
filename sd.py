399ad4da364235a271a744215ae54bdc9499a56f
morioking <morioking@gmail.com>
2016-06-16 17:13:04 +0900

    #9 #10 #11 対応

--- sd.py
+++ sd.py
@@ -11,6 +11,7 @@ import shutil
 from datetime import datetime as dt
     
 DEBUG = False
+RESULTS = 3
 
 shop_list=[
 {'shopname':u'mp3va', 		'shopurl':u'http://www.mp3va.com/'},
@@ -20,7 +21,8 @@ shop_list=[
 {'shopname':u'Juno',		'shopurl':u'http://www.junodownload.com/'},
 {'shopname':u'Hardwax','shopurl':u'https://hardwax.com/'},
 {'shopname':u'JetSet',		'shopurl':u'http://www.jetsetrecords.net/'},
-{'shopname':u'ユニオン','shopurl':u'http://diskunion.net/'}
+{'shopname':u'ユニオン','shopurl':u'http://diskunion.net/'},
+{'shopname':u'テクニーク','shopurl':u'http://www.technique.co.jp/'}
 ]
 
 
@@ -44,23 +46,25 @@ class Shop:
 		print self.name
 		# use below method to search with google
 		# https://breakingcode.wordpress.com/2010/06/29/google-search-python/
-		for url in search(keyword + ' site:' + self.url, num=5, start=0, stop=5):
-			print '  ',url
-			soup = BeautifulSoup(urllib.urlopen(url))
-			#print soup.prettify()
-			try:
-				title = soup.find('title').text
-			except AttributeError:
-				print 'AttributeError:'
-				#print 'AttributeError:'.format(e.errno, e.strerror)
-			except UnicodeEncodeError:
-				print "UnicodeEncodeError"
-			except:
-				print 'Unexpected error', sys.exc_info()[0]
+		for url in search(keyword + ' site:' + self.url, num=RESULTS, start=0, stop=RESULTS):
+			if url.find('.xml') == -1:
+				print '  ',url
+				soup = BeautifulSoup(urllib.urlopen(url))
+				#print soup.prettify()
+				try:
+					title = soup.find('title').text
+				except AttributeError:
+					print 'AttributeError:'
+				except UnicodeEncodeError:
+					print "UnicodeEncodeError"
+				except:
+					print 'Unexpected error', sys.exc_info()[0]
+				else:
+					self.disk.append(Disk(title,url))
+					print '     > ',title
 			else:
-				self.disk.append(Disk(title,url))
-				print '     > ',title
-			
+				pass
+
 class Disk:
 	def __init__(self,name,url):
 		self.name=name
@@ -87,9 +91,10 @@ def connect_jinja(shop_class_list, keyword):
 
 	tdatetime = dt.now()
 	tstr = tdatetime.strftime('%Y%m%d%H%M')
+	
 
 	html = tmpl.render({'title':title, 'disk_list':sample_list})
-	f = open(tstr+'_'+keyword+'.html', 'w')
+	f = open(tstr+'_'+keyword.replace('/', '')+'.html', 'w')
 	f.write(html.encode('utf-8'))
 	f.close()
 
